02d71cbc502b3eb5abb020f9d180d81b4a83b575
callixmiller <calmill413@gmail.com>
2023-01-11 14:23:31 -0500

    index.js file completed and passing all tests

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
 
-// Write your code here
+const num2 = 31;
+
+const num3 = 12;
+
+const num4 = 3;
+
+let multiply = (num1 * num2);
+
+let random = (Math.ceil(Math.random()));
+
+let mod = (num3 / num4);
+
+let max = (Math.max(5, 10, 20));
